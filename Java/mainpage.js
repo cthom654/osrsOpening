4f6207e145896da85d6e4b67deeb530ed14cde07
Cameron Thom <cthom654@gmail.com>
2025-10-08 20:13:16 +0100

    java

--- Java/mainpage.js
+++ Java/mainpage.js
@@ -1,3 +1,22 @@
+//==============================
+// Data Collection for monsters
+//==============================
+const monsterCache = {}; 
+//This is the notebook I write the info into (only have to get once).
+const WIKI_API_URL = 'https://oldschool.runescape.wiki/api.php';
+//This is me writing the address of the library where the info comes from (OSRS Wiki).
+console.log('Moster System Ready!');
+//This will be a print message that lets me know its working.
+
+//DEFINITIONS
+/*const = "constant" - means this won't change.
+monsterCache = the name for storage (can call it anything).
+{} = empty curly braces = an empty container.
+WIKI_API_URL = the name for the website address.
+"=" = assign/set the value.
+'https://...' = the actual web address in quotes.
+console.log() = print a message to the browser's console (developer tools).*/
+
 document.querySelectorAll('.att_method').forEach(img => {
     img.addEventListener('click', function() {
 // Remove 'selected' from all and reset their images
@@ -17,3 +36,4 @@ document.querySelectorAll('.att_method').forEach(img => {
         }
     });
 });
+
